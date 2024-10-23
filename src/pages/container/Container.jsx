d76b36c61a94793ce35c3b1716e8234065acb485
Yaroslav Mudryi <mudryi435@gmail.com>
2024-10-23 18:24:22 +0300

    feature: added default route resolver

--- src/pages/container/Container.jsx
+++ src/pages/container/Container.jsx
@@ -1,8 +1,16 @@
 import { Container as MuiContainer } from "@mui/material";
 import { NotificationsProvider } from "@toolpad/core/useNotifications";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Container = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("./contacts");
+    }
+  }, [location, navigate]);
   return (
     <NotificationsProvider>
       <MuiContainer
